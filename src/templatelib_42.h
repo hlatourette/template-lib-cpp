1350ec94e90f68911241f35821c5cd03c66333a2
Hunter LaTourette <hlatourette@gmail.com>
2020-10-13 01:22:58 -0400

    docs (uq): add javadoc comment block

--- src/templatelib_42.h
+++ src/templatelib_42.h
@@ -4,7 +4,12 @@
 #include <string>
 
 namespace templatelib {
-    // TODO doxygen
+    /**
+     * Answer the ultimate question
+     *
+     * @param question the ultimate question
+     * @return the answer to the ultimate question
+     */
     int meaningOfLifeTheUniverseAndEverything(const std::string& question);
 } // close namespace templatelib
 
